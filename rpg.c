3f0351937ba05d2f5546971fa5f43fd63f4399ac
philljj <philljj@Jordans-MacBook-Pro.local>
2018-11-25 22:09:47 -0600

    cleanup

---
+++ rpg.c
@@ -0,0 +1,444 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+#define MAX_PROCS 14
+#define MAX_ITEMS 10
+#define MAX_NAME  32
+
+void
+print_fld(const char * what,
+          const size_t amnt)
+{
+    if (amnt) {
+        printf("%s %zu\n", what, amnt);
+    }
+
+    return;
+}
+
+typedef enum {
+    MAIN_HAND = 0,
+    OFF_HAND  = 1,
+    TWO_HAND  = 2,
+    HELM      = 3,
+    SHOULDERS = 4,
+    CHEST     = 5,
+    PANTS     = 6,
+    GLOVES    = 7,
+    BOOTS     = 8,
+    RING      = 9,
+    TRINKET   = 10 
+} slot_t;
+
+const char *
+slot_to_str(slot_t s)
+{
+    switch(s) {
+    case MAIN_HAND:
+        return "main hand";
+
+    case OFF_HAND:
+        return "off hand";
+
+    case TWO_HAND:
+        return "two hand";
+
+    case HELM:
+        return "helm";
+
+    case SHOULDERS:
+        return "shoulders";
+
+    case CHEST:
+        return "chest";
+
+    case PANTS:
+        return "pants";
+
+    case GLOVES:
+        return "gloves";
+
+    case BOOTS:
+        return "boots";
+
+    case RING:
+        return "ring";
+
+    case TRINKET:
+        return "trinket";
+
+    default:
+        return "NA";
+    }
+}
+
+typedef struct {
+    size_t sta;
+    size_t str;
+    size_t agi;
+    size_t wis;
+    size_t spr;
+} attr_t;
+
+typedef struct {
+    size_t fire;
+    size_t frost;
+    size_t shadow;
+} spell_t;
+
+typedef void (*proc_t)(void);
+
+typedef struct {
+    char    name[MAX_NAME + 1];
+    attr_t  attr;
+    spell_t power;
+    spell_t resist;
+    proc_t  procs[MAX_PROCS];
+    slot_t  slot;
+    size_t  is_weapon;
+} item_t;
+
+typedef struct {
+    char    name[MAX_NAME + 1];
+    size_t  hp;
+    size_t  mp;
+    size_t  level;
+    size_t  xp;
+    attr_t  base;
+    spell_t power;
+    spell_t resist;
+    item_t  items[MAX_ITEMS];
+    size_t  have_item[MAX_ITEMS];
+} hero_t;
+
+void drain_life(void);
+void drain_mana(void);
+void restore_life(void);
+void restore_mana(void);
+
+void level_up(hero_t * h);
+void set_hp_mp(hero_t * h);
+
+item_t create_item(const char * name, const size_t level, const slot_t slot);
+size_t equip_item(hero_t * hero, const item_t * item);
+size_t attack_damage(const hero_t * h);
+size_t spell_damage(hero_t * h);
+
+hero_t roll_hero(void);
+void print_hero(hero_t * h);
+
+
+
+int
+main(int    argc,
+     char * argv[])
+{
+    hero_t h = roll_hero();
+    print_hero(&h);
+
+    return EXIT_SUCCESS;
+}
+
+
+
+hero_t
+roll_hero(void)
+{
+    hero_t h;
+    pid_t  pid;
+
+    pid = getpid();
+    srand(pid);
+
+    memset(&h, 0, sizeof(h));
+
+    h.level = 1;
+
+    h.base.sta = 6 + (rand() % 12);
+    h.base.str = 6 + (rand() % 12);
+    h.base.agi = 6 + (rand() % 12);
+    h.base.wis = 6 + (rand() % 12);
+    h.base.spr = 6 + (rand() % 12);
+
+    sprintf(h.name, "Tim the Enchanter");
+
+    h.items[CHEST] = create_item("plain shirt", h.level, CHEST);
+    h.have_item[CHEST] = 1;
+
+    h.items[PANTS] = create_item("worn pants", h.level, PANTS);
+    h.have_item[PANTS] = 1;
+
+
+    set_hp_mp(&h);
+
+    return h;
+}
+
+
+
+void
+print_hero(hero_t * h)
+{
+    printf("name: %s\n", h->name);
+    printf("\n");
+    printf("level: %zu\n", h->level);
+    printf("hp:    %zu\n", h->hp);
+    printf("mp:    %zu\n", h->mp);
+    printf("xp:    %zu\n", h->xp);
+    printf("\n");
+    printf("base attributes\n");
+    printf("  sta: %zu\n", h->base.sta);
+    printf("  str: %zu\n", h->base.str);
+    printf("  agi: %zu\n", h->base.agi);
+    printf("  wis: %zu\n", h->base.wis);
+    printf("  spr: %zu\n", h->base.spr);
+    printf("\n");
+    printf("spell power\n");
+    printf("  fire:   %zu\n", h->power.fire);
+    printf("  frost:  %zu\n", h->power.frost);
+    printf("  shadow: %zu\n", h->power.shadow);
+    printf("\n");
+    printf("spell resist\n");
+    printf("  fire:   %zu\n", h->resist.fire);
+    printf("  frost:  %zu\n", h->resist.frost);
+    printf("  shadow: %zu\n", h->resist.shadow);
+    printf("\n");
+    printf("equipment\n");
+
+    for (size_t i = 0; i < MAX_ITEMS; ++i) {
+        if (!h->have_item[i]) {
+            continue;
+        }
+
+        printf("  %s: %s\n", slot_to_str(i), h->items[i].name);
+        print_fld("    sta:", h->items[i].attr.sta);
+        print_fld("    str:", h->items[i].attr.str);
+        print_fld("    agi:", h->items[i].attr.agi);
+        print_fld("    wis:", h->items[i].attr.wis);
+        print_fld("    spr:", h->items[i].attr.spr);
+
+    }
+
+    printf("\n");
+
+
+    return;
+}
+
+
+
+item_t
+create_item(const char * name,
+            const size_t level,
+            const slot_t slot)
+{
+    // This doesn't set is_weapon.
+    item_t item;
+    size_t attr_lvl = 0;
+    size_t resist_lvl = 5;
+    size_t power_lvl = 10;
+    size_t procs_lvl = 20;
+
+    memset(&item, 0, sizeof(item));
+
+    strcpy(item.name, name);
+
+    item.slot = slot;
+
+    if (level > attr_lvl) {
+        item.attr.sta = rand() % (level + 1);
+        item.attr.str = rand() % (level + 1);
+        item.attr.agi = rand() % (level + 1);
+        item.attr.wis = rand() % (level + 1);
+        item.attr.spr = rand() % (level + 1);
+    }
+
+    if (level > resist_lvl) {
+        item.resist.fire = rand() % (level - resist_lvl);
+        item.resist.frost = rand() % (level - resist_lvl);
+        item.resist.shadow = rand() % (level - resist_lvl);
+    }
+
+    if (level > power_lvl) {
+        item.power.fire = rand() % (level - power_lvl);
+        item.power.frost = rand() % (level - power_lvl);
+        item.power.shadow = rand() % (level - power_lvl);
+    }
+
+    if (level > procs_lvl) {
+        // Do nothing for now...
+    }
+
+    return item;
+}
+
+
+
+size_t
+equip_item(hero_t *       hero,
+           const item_t * item)
+{
+    slot_t slot = item->slot;
+
+    if (hero->have_item[slot]) {
+        return 0;
+    }
+
+    hero->items[slot] = *item;
+    hero->have_item[slot] = 1;
+
+    return 1;
+}
+
+
+
+size_t
+attack_damage(const hero_t * h)
+{
+    // Attack damage is
+    //
+    //   dmg = (total strength) * (weapon multiplier)
+    //
+    // where the weapon multiplier is given by
+    //
+    //   2 handed = 2.0
+    //   1 handed = 0.8 (per hand)
+    //   unarmed  = 0.5 (per hand)
+
+    size_t dmg = 0;
+    float  mult = 0;
+    size_t str = 0;
+
+    str += h->base.str;
+
+    size_t mh = h->have_item[MAIN_HAND];
+    size_t oh = h->have_item[OFF_HAND];
+    size_t th = h->have_item[TWO_HAND];
+
+    if (!th) {
+        // Barehanded damage mult.
+        if (!mh) { mult += 0.5; }
+        if (!oh) { mult += 0.5; }
+    }
+
+    for (size_t i = 0; i < MAX_ITEMS; ++i) {
+        if (!h->have_item[i]) {
+            // Nothing to calculate.
+            continue;
+        }
+
+        str += h->items[i].attr.str;
+
+        if (!h->items[i].is_weapon) {
+            // No weapon multiplier. This could be
+            // a shield or relic.
+            continue;
+        }
+
+        switch (h->items[i].slot) {
+        case MAIN_HAND:
+            mult += 0.8;
+        break;
+
+        case OFF_HAND:
+            mult += 0.8;
+        break;
+
+        case TWO_HAND:
+            mult += 2.0;
+        break;
+
+        default:
+        break;
+        }
+    }
+
+    dmg = str * mult;
+
+    return dmg;
+}
+
+
+
+size_t
+spell_damage(hero_t * h)
+{
+    // Spell damage (and spell healing) is
+    //
+    //   dmg = (total wisdom) * (spell multiplier)
+    //
+    // where the spell multiplier is 3.
+
+    size_t dmg = 0;
+    size_t wis = 0;
+    size_t mult = 3;
+
+    for (size_t i = 0; i < MAX_ITEMS; ++i) {
+        if (!h->have_item[i]) {
+            // Nothing to calculate.
+            continue;
+        }
+
+        wis += h->items[i].attr.wis;
+    }
+
+    dmg = wis * mult;
+
+    return dmg;
+}
+
+
+
+void
+level_up(hero_t * h)
+{
+    ++(h->level);
+    ++(h->base.sta);
+    ++(h->base.str);
+    ++(h->base.agi);
+    ++(h->base.wis);
+    ++(h->base.spr);
+
+    h->xp = 0;
+
+    return;
+}
+
+
+
+void
+set_hp_mp(hero_t * h)
+{
+    // hp =  5 * stamina
+    // mp = 10 * wisdom
+    size_t stamina = h->base.sta;
+    size_t wisdom = h->base.wis;
+
+    for (size_t i = 0; i < MAX_ITEMS; ++i) {
+        if (!h->have_item[i]) {
+            // Nothing to do.
+            continue;
+        }
+
+        stamina += h->items[i].attr.sta;
+        wisdom += h->items[i].attr.wis;
+    }
+
+    h->hp =  5 * stamina;
+    h->mp = 10 * wisdom;
+
+    return;
+}
+
+
+
+
+void
+drain_life(void)
+{
+    return;
+}
+
+
